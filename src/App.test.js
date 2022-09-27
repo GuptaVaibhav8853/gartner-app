60f215da6df2ed7fbcc058f47ac1eeb1a9809df6
vaibhav <vaibhavguptammm@gmail.com>
2022-09-27 11:58:46 +0530

    test gile modified

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,12 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('Test Suite', () => {
-  test('renderring correctly', () => {
+  test('rendering app correctly', () => {
     const wrapper = render(<App />);
     expect(wrapper).toMatchSnapshot();
   });
 
-  test('renderring header element correctly', () => {
+  test('rendering heading element correctly', () => {
     const { getByTestId } = render(<App />);
     const result_header = getByTestId('result')
     expect(result_header).toBeInTheDocument();
